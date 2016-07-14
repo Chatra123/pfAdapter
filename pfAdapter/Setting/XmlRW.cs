12691005b441ad1913b7f03ba4c79794a9b73a2c
c <>
2016-07-14 20:04:47 +0900

    FileRW XmlRW

--- pfAdapter/Setting/XmlRW.cs
+++ pfAdapter/Setting/XmlRW.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * 最終更新日　16/06/21
+ * 最終更新日　16/07/14
  * 
  * 概要
  *   ＸＭＬファイルの読み書き
@@ -21,13 +21,13 @@ namespace OctNov.IO
   internal class XmlRW
   {
     /// <summary>
-    /// 復元  XmlFile　→　T object
+    /// 復元    Xml file  -->  T object
     /// </summary>
     /// <typeparam name="T">復元するクラス</typeparam>
     /// <param name="filename">XMLファイル名</param>
     /// <returns>復元したオブジェクト</returns>
     /// <remarks>
-    ///   ○スペースだけのstringを維持
+    ///   ◇スペースだけのstringを維持
     ///     XmlSerializerで直接読み込むとスペースだけの文字列が空文字になる。
     ///   　XmlDocumentを経由し、PreserveWhitespace = true;で変換する。
     /// </remarks>
@@ -35,18 +35,18 @@ namespace OctNov.IO
     {
       if (File.Exists(filename) == false) return default(T);
 
-      for (int i = 1; i <= 2; i++)
+      for (int i = 1; i <= 5; i++)
       {
         try
         {
-          //                                                                  共有設定  FileShare.Read
-          using (var fstream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+          //                                                                 共有設定  FileShare.Read
+          using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
           {
-            //                                                       UTF-8 bom
-            using (var streader = new StreamReader(fstream, Encoding.UTF8))
+            //                                                    UTF-8 bom
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
             {
               //XmlDocument経由で変換する。
-              string xmltext = streader.ReadToEnd();
+              string xmltext = reader.ReadToEnd();
               var doc = new XmlDocument();
               doc.PreserveWhitespace = true;
               doc.LoadXml(xmltext);
@@ -59,22 +59,23 @@ namespace OctNov.IO
         }
         catch (IOException ioexc)
         {
-          if (2 <= i)
+          if (5 <= i)
           {
             var msg = new StringBuilder();
             msg.AppendLine(ioexc.Message);
             msg.AppendLine("別のプロセスが使用中のためxmlファイルを読み込めません。");
-            msg.AppendLine("path:" + filename);
+            msg.AppendLine("name:" + filename);
             msg.AppendLine();
             throw new IOException(msg.ToString());
           }
         }
         catch (Exception exc)
         {
-          //ＸＭＬ作成失敗
-          //フォーマット、シリアル属性を確認してください。
+          //ＸＭＬ読込失敗
+          //フォーマット、シリアル属性を確認
           throw exc;
         }
+
         System.Threading.Thread.Sleep(50);
       }
 
@@ -83,25 +84,25 @@ namespace OctNov.IO
 
 
     /// <summary>
-    /// 保存  T object  →  XmlFile
+    /// 保存    T object  -->  Xml file
     /// </summary>
     /// <typeparam name="T">保存するクラス</typeparam>
     /// <param name="filename">XMLファイル名</param>
     /// <returns>保存が成功したか</returns>
     public static bool Save<T>(string filename, T setting)
     {
-      for (int i = 1; i <= 2; i++)
+      for (int i = 1; i <= 3; i++)
       {
         try
         {
-          //                                                                     共有設定  FileShare.None
-          using (var fstream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None))
+          //                                                                    共有設定  FileShare.None
+          using (var stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None))
           {
-            //                                                       UTF-8 bom
-            using (var stwriter = new StreamWriter(fstream, Encoding.UTF8))
+            //                                                    UTF-8 bom
+            using (var writer = new StreamWriter(stream, Encoding.UTF8))
             {
               var serializer = new XmlSerializer(typeof(T));
-              serializer.Serialize(stwriter, setting);
+              serializer.Serialize(writer, setting);
               return true;
             }
           }
@@ -120,31 +121,6 @@ namespace OctNov.IO
 
       return false;
     }
-
-
-    /// <summary>
-    /// 保存  T object  →  XmlFile　＆　バックアップ
-    /// </summary>
-    /// <typeparam name="T">保存するクラス</typeparam>
-    /// <param name="filename">XMLファイル名</param>
-    /// <returns>保存が成功したか</returns>
-    public static bool Save_withBackup<T>(string filename, T settings)
-    {
-      if (File.Exists(filename))
-      {
-        try
-        {
-          if (File.Exists(filename + ".sysbak"))
-            File.Delete(filename + ".sysbak");
-          File.Move(filename, filename + ".sysbak");
-        }
-        catch { /* do nothing */ }
-      }
-
-      return Save(filename, settings);
-    }
-
-
 
   }
 }
--- pfAdapter/Text/FileRW.cs
+++ pfAdapter/Text/FileRW.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * 最終更新日
- *   16/04/06
+ *   16/07/12
  *  
  * 概要
  *   テキストファイルの読み書きにファイル共有設定をつける。
@@ -50,41 +50,20 @@ namespace OctNov.IO
     /// <summary>
     /// ファイルストリーム作成
     /// </summary>
-    /// <param name="path">読込むファイルのパス</param>
-    /// <param name="LargeSwitch">大きなサイズのファイル読込みを許可するか</param>
     /// <returns>
     /// 成功　→　FileStream
     /// 失敗　→　null
     /// </returns>
-    public static FileStream CreateStream(string path, bool LargeSwitch = false)
+    public static FileStream CreateStream(string path)
     {
-      //１０ＭＢ以上なら、メモリーに読み込む前に例外をスロー
-      //    d2v、srt、txtなら１０ＭＢ以上になることはない
-      //    lwi だけ１０ＭＢを超える。
       try
       {
-        var finfo = new FileInfo(path);
-
-        if (LargeSwitch == false
-          && finfo.Exists && 10 * 1024 * 1024 <= finfo.Length)
-          throw new Exception("Large file, gt 10MB: " + path);
-      }
-      catch
-      {
-        return null;
-      }
-
-
-      FileStream stream = null;
-      try
-      {
-        stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);        //FileShare
+        var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         return stream;
       }
       catch
       {
-        //ファイルチェックから読込みの間に削除された。srtだと発生する可能性がある。
-        if (stream != null) stream.Close();
+        //ファイルチェックからCreateStream()の間に削除された。srtだと発生する可能性がある。
         return null;
       }
     }
@@ -97,12 +76,12 @@ namespace OctNov.IO
     /// <returns>読み込んだテキスト</returns>
     public static List<string> ReadAllLines(string path, Encoding enc = null)
     {
-      //create reader
+      //create
       var stream = CreateStream(path);
       if (stream == null) return null;
       enc = enc ?? TextEnc.Shift_JIS;
 
-      //read file
+      //read
       var text = new List<string>();
 
       using (var reader = new StreamReader(stream, enc))
@@ -120,11 +99,11 @@ namespace OctNov.IO
     /// <returns>読み込んだバイナリ</returns>
     public static byte[] ReadBytes(string path)
     {
-      //create reader
+      //create
       var stream = CreateStream(path);
       if (stream == null) return null;
 
-      //read file
+      //read
       var readData = new List<byte>();
 
       using (var reader = new BinaryReader(stream))
@@ -132,7 +111,7 @@ namespace OctNov.IO
         byte[] readOne = null;
         while (true)
         {
-          readOne = reader.ReadBytes(256 * 1024);
+          readOne = reader.ReadBytes(32 * 1024);
           if (readOne.Count() == 0) break;
           else
             readData.AddRange(readOne);
@@ -142,7 +121,6 @@ namespace OctNov.IO
     }
 
     //=====================================
-    //instance
     // lwi 読込み用
     //=====================================
     private StreamReader reader = null;
@@ -155,9 +133,13 @@ namespace OctNov.IO
     public FileR(string path, Encoding enc = null)
     {
       enc = enc ?? TextEnc.Shift_JIS;
-      var stream = CreateStream(path, true);
+      var stream = CreateStream(path);
       if (stream != null)
         reader = new StreamReader(stream, enc);
+    }
+    ~FileR()
+    {
+      Close();
     }
 
     /// <summary>
@@ -170,27 +152,27 @@ namespace OctNov.IO
     }
 
     /// <summary>
-    /// Ｎ行読込む。
+    /// Ｎ行読込む
     /// </summary>
     /// <param name="NLines">読み込む最大行数</param>
     /// <returns>
     /// 読み込んだテキスト、０～Ｎ行
-    /// NLinesに満たない場合は読み込めた分だけ返す
+    /// NLinesに満たない場合は読み込めた分だけ返す。
     /// </returns>
     public List<string> ReadNLines(int NLines)
     {
-      var textlist = new List<string>();
+      var text = new List<string>();
       for (int i = 0; i < NLines; i++)
       {
         string line = reader.ReadLine();
-        if (line != null) textlist.Add(line);
+        if (line != null) text.Add(line);
         else break;
       }
-      return textlist;
+      return text;
     }
 
     /// <summary>
-    /// アセンブリ内のリソース読込み。
+    /// アセンブリ内のリソース読込み
     /// </summary>
     /// <remarks>
     /// リソースが存在しないとnew StreamReader(null,enc)で例外
@@ -226,18 +208,17 @@ namespace OctNov.IO
   internal class FileW
   {
     /// <summary>
-    /// ライター作成　　パス形式が無効なら例外
+    /// ライター作成　　ファイルパス形式でないと例外
     /// </summary>
     /// <param name="path">作成するファイルパス</param>
     /// <param name="enc">文字エンコードの指定。デフォルトShift-JIS</param>
     /// <returns></returns>
-    public static StreamWriter CreateWriter(string path, Encoding enc = null)
-    {
-      enc = enc ?? TextEnc.Shift_JIS;
-      var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read);        //FileShare
-      var writer = new StreamWriter(stream, enc);                                                  //文字エンコード
-      return writer;
-    }
+    //public static StreamWriter CreateWriter(string path, Encoding enc)
+    //{
+    //  var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read);        //FileShare
+    //  var writer = new StreamWriter(stream, enc);                                                  //文字エンコード
+    //  return writer;
+    //}
 
     /// <summary>
     /// バイナリ追記
@@ -252,7 +233,6 @@ namespace OctNov.IO
     }
 
     //=====================================
-    //instance
     // lwi 書込み用
     //=====================================
     private StreamWriter writer;
@@ -261,10 +241,16 @@ namespace OctNov.IO
     /// Constructor   FileShare.Read
     /// </summary>
     /// <param name="path">書込み対象のファイルパス</param>
-    /// <param name="enc">文字エンコードの指定。デフォルトShift-JIS</param>
+    /// <param name="enc">文字エンコードの指定</param>
     public FileW(string path, Encoding enc = null)
     {
-      writer = CreateWriter(path, enc);
+      enc = enc ?? TextEnc.Shift_JIS;
+      var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read);
+      writer = new StreamWriter(stream, enc);
+    }
+    ~FileW()
+    {
+      Close();
     }
 
     /// <summary>
@@ -287,17 +273,15 @@ namespace OctNov.IO
     /// <summary>
     /// テキスト書込み
     /// </summary>
-    /// <param name="line">書込むテキスト　１行</param>
-    public void WriteText(string line)
+    public void WriteLine(string line)
     {
-      WriteText(new string[] { line });
+      WriteLine(new string[] { line });
     }
 
     /// <summary>
     /// テキスト書込み
     /// </summary>
-    /// <param name="text">>書込むテキスト</param>
-    public void WriteText(IEnumerable<string> text)
+    public void WriteLine(IEnumerable<string> text)
     {
       foreach (var line in text)
         writer.WriteLine(line);
